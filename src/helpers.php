4644a69d564cc9bb881b0b1a7b3ec7fda23d2a57
Administrator <howmao@qq.com>
2024-06-05 10:46:53 +0800

    add method

--- src/helpers.php
+++ src/helpers.php
@@ -2,34 +2,37 @@
 
 use \Createlinux\OAuth\Client;
 
+const LITCHI_AUTH_CLIENT_ID_NAME = 'LITCHI_AUTH_CLIENT_ID';
+const LITCHI_AUTH_CLIENT_APP_URL_NAME = 'LITCHI_AUTH_CLIENT_APP_URL';
+const LITCHI_AUTH_SERVER_APP_URL_NAME = 'LITCHI_AUTH_SERVER_APP_URL';
 if (!function_exists('get_litchi_auth_client_key')) {
-    function get_litchi_auth_client_key()
+    function get_litchi_auth_client_id()
     {
-        return 'LITCHI_AUTH_CLIENT_ID';
+        return getenv(LITCHI_AUTH_CLIENT_ID_NAME);
     }
 }
 if (!function_exists('get_litchi_auth_client_app_url')) {
     function get_litchi_auth_client_app_url()
     {
-        return "LITCHI_AUTH_CLIENT_APP_URL";
+        return getenv(LITCHI_AUTH_CLIENT_APP_URL_NAME);
     }
 }
 if (!function_exists('get_litchi_auth_server_app_url')) {
     function get_litchi_auth_server_app_url()
     {
-        return "LITCHI_AUTH_SERVER_APP_URL";
+        return getenv(LITCHI_AUTH_SERVER_APP_URL_NAME);
     }
 }
 if (!function_exists('create_litchi_oauth_client')) {
     function create_litchi_oauth_client($AUTH_SERVER_APP_URL = '', $AUTH_CLIENT_APP_URL = '')
     {
         $oauthClient = new \Createlinux\OAuth\Client();
-        $oauthClient->setClientId(getenv(get_litchi_auth_client_key()));
+        $oauthClient->setClientId(get_litchi_auth_client_id());
         if ($AUTH_CLIENT_APP_URL) {
-            $oauthClient->setOpenAuthClientURIForDev(getenv(get_litchi_auth_client_app_url()));
+            $oauthClient->setOpenAuthClientURIForDev(get_litchi_auth_client_app_url());
         }
         if ($AUTH_SERVER_APP_URL) {
-            $oauthClient->setOpenAuthServerURIForDev(getenv(get_litchi_auth_server_app_url()));
+            $oauthClient->setOpenAuthServerURIForDev(get_litchi_auth_server_app_url());
         }
         return $oauthClient;
     }
