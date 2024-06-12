7f0c9e174e4ab5e601414a5afec1d2091fb0ff91
Administrator <howmao@qq.com>
2024-06-12 15:46:49 +0800

    修改env名称

--- src/helpers.php
+++ src/helpers.php
@@ -4,9 +4,9 @@ use \Createlinux\OAuth\Client;
 
 const LITCHI_AUTH_CLIENT_ID_NAME = 'LITCHI_AUTH_CLIENT_ID';
 const LITCHI_AUTH_CLIENT_SECRET = 'LITCHI_AUTH_CLIENT_SECRET';
-const LITCHI_AUTH_CLIENT_APP_URL_NAME = 'LITCHI_AUTH_CLIENT_APP_URL';
-const LITCHI_AUTH_SERVER_APP_URL_NAME = 'LITCHI_AUTH_SERVER_APP_URL';
-const LITCHI_AUTH_REDIRECT_URI = 'LITCHI_AUTH_REDIRECT_URI';
+const LITCHI_AUTH_CENTER_CLIENT_URL = 'LITCHI_AUTH_CENTER_CLIENT_APP_URL';
+const LITCHI_AUTH_CENTER_SERVER_URL = 'LITCHI_AUTH_CENTER_SERVER_APP_URL';
+const LITCHI_AUTH_CLIENT_REDIRECT_URI = 'LITCHI_AUTH_CLIENT_REDIRECT_URI';
 
 if (!function_exists('get_litchi_auth_client_key')) {
     function get_litchi_auth_client_id()
@@ -14,22 +14,22 @@ if (!function_exists('get_litchi_auth_client_key')) {
         return getenv(LITCHI_AUTH_CLIENT_ID_NAME);
     }
 }
-if (!function_exists('get_litchi_auth_client_app_uri')) {
-    function get_litchi_auth_client_app_uri()
+if (!function_exists('get_litchi_auth_center_client_uri')) {
+    function get_litchi_auth_center_client_uri()
     {
         if (getenv('APP_ENV') === 'production') {
             return 'https://auth.lizhiruanjian.com';
         }
-        return getenv(LITCHI_AUTH_CLIENT_APP_URL_NAME);
+        return getenv(LITCHI_AUTH_CENTER_CLIENT_URL);
     }
 }
-if (!function_exists('get_litchi_auth_server_app_uri')) {
-    function get_litchi_auth_server_app_uri()
+if (!function_exists('get_litchi_auth_center_server_uri')) {
+    function get_litchi_auth_center_server_uri()
     {
         if (getenv('APP_ENV') === 'production') {
             return 'https://auth-api.lizhiruanjian.com';
         }
-        return getenv(LITCHI_AUTH_SERVER_APP_URL_NAME);
+        return getenv(LITCHI_AUTH_CENTER_SERVER_URL);
     }
 }
 if (!function_exists('create_litchi_oauth_client')) {
@@ -46,9 +46,9 @@ if (!function_exists('get_litchi_auth_client_secret')) {
     }
 }
 
-if (!function_exists('get_litchi_auth_redirect_uri')) {
-    function get_litchi_auth_redirect_uri()
+if (!function_exists('get_litchi_auth_client_redirect_uri')) {
+    function get_litchi_auth_client_redirect_uri()
     {
-        return getenv(LITCHI_AUTH_REDIRECT_URI);
+        return getenv(LITCHI_AUTH_CLIENT_REDIRECT_URI);
     }
 }
